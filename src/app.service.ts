c52202b0231a875c085deac361044bae02a6e182
Yossef-Elshafey <yssef0472@gmail.com>
2024-09-23 17:15:42 +0300

    response ordering

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,7 @@ import { LanguageEnum } from './dto/language.enum';
 import { BulkCreateWordDto, CreateWordDto } from './dto/input/create-word.dto';
 import { Word } from './raven/entities/word.entity';
 import { UpdateWordDto } from './dto/input/update-word.dto';
+import { repl } from '@nestjs/core';
 
 @Injectable()
 export class AppService {
@@ -15,6 +16,37 @@ export class AppService {
       wordReg = `\\b${wordReg}\\b`;
     }
     return wordReg;
+  }
+
+  private forEgy(res: Word[], lang: LanguageEnum, input: string) {
+    let moveUp = 0;
+    for (const [idx, doc] of Object.entries(res)) {
+      const curr = doc[lang][0];
+      if (curr.Word.length === input.length) {
+        this.swapping({ change: moveUp, with: +idx }, res);
+        moveUp++;
+      }
+    }
+    return res;
+  }
+
+  private swapping(idxs: { change: number; with: number }, res: Word[]) {
+    const stash = res[idxs.change];
+    res[idxs.change] = res[idxs.with];
+    res[idxs.with] = stash;
+    return res;
+  }
+
+  private forAra(res: Word[], lang: LanguageEnum) {
+    let moveUp = 0;
+    for (const [idx, doc] of Object.entries(res)) {
+      const curr = doc[lang].map((obj: { Word: string }) => obj.Word);
+      if (curr.length === 1) {
+        this.swapping({ change: moveUp, with: +idx }, res);
+        moveUp++;
+      }
+    }
+    return res;
   }
 
   async search(lang: LanguageEnum, word: string) {
@@ -31,7 +63,11 @@ export class AppService {
       .closeSubclause()
       .take(10)
       .all();
-    return this.toDto(resFullTextSearch as Word[]);
+    if (lang === 'Egyptian') {
+      return this.forEgy(this.toDto(resFullTextSearch as Word[]), lang, word);
+    } else {
+      return this.forAra(this.toDto(resFullTextSearch as Word[]), lang);
+    }
   }
 
   private toDto(res: Word[]): Word[] {
