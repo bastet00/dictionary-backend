be975cdc1ebff07eadaffddb5d9095d99bcf0c20
Yossef-Elshafey <yssef0472@gmail.com>
2024-10-22 06:56:58 +0300

    change regex, test

--- src/common/custom-pipes/sanitizeSymbolPipe.ts
+++ src/common/custom-pipes/sanitizeSymbolPipe.ts
@@ -4,8 +4,8 @@ import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 export class SanitizeSpecialCharsPipe implements PipeTransform {
   transform(value: string) {
     if (value) {
-      // @Returns: chars and numbers ( include unicode for arabic chars )
-      return value.replace(/[^a-zA-Z0-9\u0600-\u06FF]/g, '');
+      // @Returns: chars, numbers and spaces
+      return value.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
     }
 
     throw new BadRequestException('word was not provided');
