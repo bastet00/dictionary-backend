f6a91e5cd20e5ca161b40a671237f141b3342fd8
Amro <amranwar945@gmail.com>
2024-10-26 20:05:49 +0200

    Add pipelines

--- src/word/word.service.ts
+++ src/word/word.service.ts
@@ -1,8 +1,4 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { RavendbService } from '../raven/raven.service';
 import { LanguageEnum } from '../dto/language.enum';
 import { BulkCreateWordDto, CreateWordDto } from '../dto/input/create-word.dto';
