1d764315205aa5084c530a6287f55937b6620645
Yossef-Elshafey <yssef0472@gmail.com>
2024-10-31 04:39:12 +0300

    rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,17 @@ import { PrivacyPolicyModule } from './privacy-policy/privacy-policy.module';
 import { AdminModule } from './admin/admin.module';
 import { WordModule } from './word/word.module';
 import { AppController } from './app.controller';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 1000 * 1,
+        limit: 5,
+      },
+    ]),
     ConfigModule.forRoot({ envFilePath: '.env' }),
     WordSuggestionModule,
     LoginModule,
@@ -17,7 +25,13 @@ import { AppController } from './app.controller';
     AdminModule,
     WordModule,
   ],
-  providers: [RavendbService],
+  providers: [
+    RavendbService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
