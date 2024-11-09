37471cff47b590fc865660e01eca614cce002ee6
Yossef-Elshafey <yssef0472@gmail.com>
2024-11-09 08:53:58 +0200

    object contains patterns of db calls

--- src/word/word.service.ts
+++ src/word/word.service.ts
@@ -8,25 +8,38 @@ import { Word } from '../raven/entities/word.entity';
 export class WordService {
   constructor(private readonly ravendbService: RavendbService) {}
 
-  private applyRegex(word: string) {
-    let wordReg = word.replace(/ا/g, '[اأإ]');
-    if (word.length <= 2) {
-      wordReg = `\\b${wordReg}\\b`;
-    }
-    return wordReg;
+  private searchPatterns(word: string) {
+    const origWordLength = word.length;
+    const lettersSwapping = word.replace(/ا/g, '[اأإ]');
+    const patterns = {
+      exactOrGlobalSearch:
+        origWordLength <= 2
+          ? this.exactRegexMatch(lettersSwapping)
+          : `.*${lettersSwapping}.*`,
+      wordSplited: `.*${this.splitWord(lettersSwapping)}.*`,
+    };
+    return patterns;
+  }
+
+  private splitWord(word: string) {
+    return word.split(' ')[0];
+  }
+
+  private exactRegexMatch(word: string) {
+    return `^\\b${word}\\b$`;
   }
 
   async search(lang: LanguageEnum, word: string) {
-    const regSearch = this.applyRegex(word);
+    const patterns = this.searchPatterns(word);
     const session = this.ravendbService.session();
     const resFullTextSearch = await session
       .query({ collection: 'word' })
       .openSubclause()
-      .whereRegex(`${lang}.word`, `^${regSearch}$`)
+      .whereRegex(`${lang}.word`, patterns.exactOrGlobalSearch)
       .closeSubclause()
       .orElse()
       .openSubclause()
-      .whereRegex(`${lang}.word`, `.*${regSearch}.*`)
+      .whereRegex(`${lang}.word`, patterns.wordSplited)
       .closeSubclause()
       .take(10)
       .orderByScore()
