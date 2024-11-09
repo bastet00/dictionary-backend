d6567e4bab9c6cf96dbb44461dc69592343714cb
Yossef-Elshafey <yssef0472@gmail.com>
2024-11-09 13:13:50 +0200

    multi word search regex

--- src/word/word.service.ts
+++ src/word/word.service.ts
@@ -8,25 +8,54 @@ import { Word } from '../raven/entities/word.entity';
 export class WordService {
   constructor(private readonly ravendbService: RavendbService) {}
 
-  private applyRegex(word: string) {
-    let wordReg = word.replace(/ا/g, '[اأإ]');
-    if (word.length <= 2) {
-      wordReg = `\\b${wordReg}\\b`;
+  searchPatterns(word: string) {
+    const origWordLength = word.length;
+    const lettersSwapping = word.replace(/ا/g, '[اأإ]');
+    const patterns = {
+      exactOrGlobalSearch:
+        origWordLength <= 3
+          ? this.exactRegexMatch(lettersSwapping)
+          : `.*${lettersSwapping}.*`,
+      wordSplited: `${this.splitWord(lettersSwapping)}`,
+    };
+    return patterns;
+  }
+
+  private splitWord(word: string) {
+    const words = word.split(' ');
+    if (words.length > 1) {
+      let concat = '';
+
+      words.map((word, i) => {
+        concat += `.*${word}.*|`;
+        if (i === 3) {
+          return concat;
+        }
+      });
+
+      return concat.slice(0, concat.length - 1);
     }
-    return wordReg;
+
+    return `.*${words[0].slice(0, Math.trunc(words[0].length / 2))}.*`;
+  }
+
+  private exactRegexMatch(word: string) {
+    return `^\\b${word}\\b$`;
   }
 
   async search(lang: LanguageEnum, word: string) {
-    const regSearch = this.applyRegex(word);
+    const patterns = this.searchPatterns(word);
+    console.log(patterns);
+
     const session = this.ravendbService.session();
     const resFullTextSearch = await session
       .query({ collection: 'word' })
       .openSubclause()
-      .whereRegex(`${lang}.word`, `^${regSearch}$`)
+      .whereRegex(`${lang}.word`, patterns.exactOrGlobalSearch)
       .closeSubclause()
       .orElse()
       .openSubclause()
-      .whereRegex(`${lang}.word`, `.*${regSearch}.*`)
+      .whereRegex(`${lang}.word`, patterns.wordSplited)
       .closeSubclause()
       .take(10)
       .orderByScore()
