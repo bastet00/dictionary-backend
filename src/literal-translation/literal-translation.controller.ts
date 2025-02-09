2dba912096ef29489f028767f16aa2a7d55385ed
Amro <amranwar945@gmail.com>
2025-02-09 11:55:07 +0100

    literal-translation path directly

--- src/literal-translation/literal-translation.controller.ts
+++ src/literal-translation/literal-translation.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Query } from '@nestjs/common';
 import { LiteralTranslationService } from './literal-translation.service';
 import { LiteralTranslationResultsDto } from './dto/literal-translation-results.dto';
 
-@Controller('api/v1/literal-translation')
+@Controller(['api/v1/literal-translation', 'literal-translation'])
 export class LiteralTranslationController {
   constructor(
     private readonly literalTranslationService: LiteralTranslationService,
