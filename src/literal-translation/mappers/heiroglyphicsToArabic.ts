2a52734496d2d2dc91579c23137dc8936e0bbdc3
Amro <amranwar945@gmail.com>
2025-03-06 22:39:52 +0100

    Add one more caching layer

--- src/literal-translation/mappers/heiroglyphicsToArabic.ts
+++ src/literal-translation/mappers/heiroglyphicsToArabic.ts
@@ -6,13 +6,15 @@ export const hieroglyphicsToArabic = cachedHieroglyphicsToArabic
   : getHieroglyphicsToArabicMapper();
 
 function getHieroglyphicsToArabicMapper(): LiteralTranslationLangMapper {
-  const swapped = {};
+  if (cachedHieroglyphicsToArabic) {
+    return cachedHieroglyphicsToArabic;
+  }
+  cachedHieroglyphicsToArabic = {};
   for (const [k, v] of Object.entries(arabicToHieroglyphics)) {
     // avoid overwrite
-    if (!swapped[v]) {
-      swapped[v] = k;
+    if (!cachedHieroglyphicsToArabic[v]) {
+      cachedHieroglyphicsToArabic[v] = k;
     }
   }
-  cachedHieroglyphicsToArabic = swapped;
   return cachedHieroglyphicsToArabic;
 }
