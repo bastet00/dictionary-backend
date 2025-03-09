47071325ce33360f2e6d1d9bff3aa644694d5f7e
Amro <amranwar945@gmail.com>
2025-03-09 23:05:10 +0100

    Add db module

--- src/translation/translation.module.ts
+++ src/translation/translation.module.ts
@@ -3,9 +3,10 @@ import { TranslationService } from './translation.service';
 import { TranslationController } from './translation.controller';
 import { WordModule } from 'src/word/word.module';
 import { SentenceService } from './sentence.service';
+import { RavenModule } from 'src/raven/raven.module';
 
 @Module({
-  imports: [WordModule],
+  imports: [WordModule, RavenModule],
   controllers: [TranslationController],
   providers: [TranslationService, SentenceService],
 })
