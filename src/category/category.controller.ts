7d5d857d32890646e08a282ddcd8e72e191beee9
AmrAnwar <amranwar945@gmail.com>
2025-07-13 15:16:21 +0200

    Update CategoryController to support multiple route paths
    This change modifies the CategoryController to handle requests from both 'api/v1/category' and 'category' routes, enhancing the flexibility of the API endpoints for category management.

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { CategoryService } from './category.service';
 
-@Controller('api/v1/category')
+@Controller(['api/v1/category', 'category'])
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
