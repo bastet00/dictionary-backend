60c192103023c4a6d6fc0f3b8e78c489f64913f1
AmrAnwar <amranwar945@gmail.com>
2025-07-14 19:30:35 +0200

    Add caching support in Category module and controller
    This update integrates caching functionality into the Category module and controller by utilizing the @nestjs/cache-manager package. The CategoryController is now equipped with a CacheInterceptor and a CacheTTL of 24 hours for the getCategoryWords endpoint, while the CategoryModule registers a CacheModule with a specified TTL and maximum items. This enhancement improves performance by reducing redundant data retrieval.

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
 import { RavenModule } from 'src/raven/raven.module';
 
 @Module({
-  imports: [RavenModule],
+  imports: [
+    CacheModule.register({
+      ttl: 24 * 60 * 60 * 1000, // 24 hours in milliseconds
+      max: 100, // maximum number of items in cache
+    }),
+    RavenModule,
+  ],
   controllers: [CategoryController],
   providers: [CategoryService],
 })
